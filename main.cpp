4d04013e454f5cc052f76bfc7befb0b01d616d80
abdulhathidev <57032182+abdulhathidev@users.noreply.github.com>
2020-03-05 01:23:12 -0800

    C++ code checking

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int max(int a, int b)
+{
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+int main()
+{
+    int a = 10;
+    int m = max(10, 11);
+    std::cout << "hi";
+    return 1;
+}
