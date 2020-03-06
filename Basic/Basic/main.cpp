8e23bd7e46fd9b2cb305619fea115ab4cc13b624
abdulhathidev <57032182+abdulhathidev@users.noreply.github.com>
2020-03-06 01:54:01 -0800

    c++ code checkin

---
+++ Basic/Basic/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Basic
+//
+//  Created by Abdul Hathi on 3/5/20.
+//  Copyright © 2020 Abdul Hathi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
